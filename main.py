d5a0cf4e039ef61c3bfbf1567868fa5d322a3471
TechWithAkash <vishwakarmaakashav17@gmail.com>
2024-05-05 18:05:52 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,84 @@
+
+
+# pip3 install flask opencv-python
+import cv2
+import os
+from flask import Flask, render_template, request, flash, jsonify
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'webp', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def processImage(filename, operation):
+    print(f"The operation is {operation} and filename is {filename}")
+    img = cv2.imread(f"uploads/{filename}")
+    match operation:
+        case "cgray":
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            newFilename = f"static/{filename}"
+            cv2.imwrite(newFilename, imgProcessed)
+            return newFilename
+        case "cwebp": 
+            newFilename = f"static/{filename.split('.')[0]}.webp"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case "cjpg": 
+            newFilename = f"static/{filename.split('.')[0]}.jpg"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case "cpng": 
+            newFilename = f"static/{filename.split('.')[0]}.png"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case "cjpeg": 
+            newFilename = f"static/{filename.split('.')[0]}.jpeg"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+    pass
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/edit", methods=["POST"])
+def edit():
+    if request.method == "POST": 
+        operation = request.form.get("operation")
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'})
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'})
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            new = processImage(filename, operation)
+            converted_url = f"/{new}"  # Assuming the converted image is saved in the 'static' folder
+            return jsonify({'success': True, 'converted_url': converted_url})
+    return jsonify({'error': 'Invalid request'})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
